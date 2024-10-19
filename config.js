1a88e1d9bdb6dfa8de9efb8fd1e1e922db590ef0
Malaka-MRT <malakabot1@gmail.com>
2024-10-19 15:15:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-MONGODB: process.env.MONGODB || "enter mongodb here",
+SESSION_ID: process.env.SESSION_ID || "SM1AXJgQ#jwG5514NLFYmXIRyrRtGG4FwPs27CiUkh62IlQFBIMY",
+MONGODB: process.env.MONGODB || "mongodb://mongo:GoAcDbePrmPNKPlSpKkGrraMHJQVHpcZ@autorack.proxy.rlwy.net:15164",
 };
